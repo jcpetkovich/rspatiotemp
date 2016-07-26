6621128df8c9b31f02771d377bf813a0aa4ebb66
allenhuang97 <allen.weixi.huang@gmail.com>
2016-07-26 17:00:21 -0400

    EOL using STFT

--- R/RULFeature.R
+++ R/RULFeature.R
@@ -1,15 +1,19 @@
+#' Compute Mean Arithmetic PSD of the data
+#' @title Mean Arithmetic PSD (meanPSD)
+#' @param data The data to have its mean arithmetic psd computed
+#' @return The Mean Arithmetic PSD of the data
+#' @export
 meanPSD <- function(data){
   result = sum(abs(fft(data)))/length(data)
   result = 20*log10(result/(10e-5))
   return(result)
 }
 
-pca <- function(data){
-  data.pca = prcomp(data,scale. = TRUE,center = TRUE)
-  indexMax = which.max(data.pca$sdev)
-  return(data.pca$x[,indexMax])
-}
-
+#' Compute Peak-to-peak value of the data
+#' @title Peak-to-Peak (pk2pk)
+#' @param data The data to have its peak-to-peak value computed
+#' @return The Peak-to-Peak Value of the data
+#' @export
 pk2pk <- function(data){
   data.diff = diff(data)
   zeros = which(data.diff == 0)
@@ -46,6 +50,12 @@ pk2pk <- function(data){
   return(peakMax-peakMin)
 }
 
+#' Compute the AutoRegressive model values of the data
+#' @title AutoRegressive model values (arModel)
+#' @param data The data to have its autoregressive model values computed
+#' @param order.max The maximum number of coefficients the ar model can compute
+#' @return the AutoRegressive model values of the data
+#' @export
 arModel <- function(data, order.max){
   len = length(data)
   arCoef = ar(data,order.max = order.max)$ar
@@ -59,17 +69,42 @@ arModel <- function(data, order.max){
   return(values)
 }
 
+#' Compute the entropy of the data
+#' @title Entropy (entropyR)
+#' @param data The data to have its entropy computed
+#' @param binNum The binNum parameter for entropy::discretize function
+#' @return The entropy value of the data
+#' @export
 entropyR <- function(data, binNum){
   data.dis = entropy::discretize(data,binNum)
   return(entropy::entropy(data.dis))
 }
 
+#' Compute Mutual Information value of the data
+#' @title Mutual Information (mutInfo)
+#' @param data1 The first set of data to have its mutual information computed
+#' @param data2 The second set of data to have its mutual information computed
+#' @param binNum1 The binNum parameter for entropy::discretize2d function for the first data set
+#' @param binNum2 The binNum parameter for entropy::discretize2d function for the second data set
+#' @return The Mutual Information value between the two data sets
+#' @export
 mutInfo <- function(data1, data2, binNum1, binNum2){
   data.dis = entropy::discretize2d(data1,data2,binNum1,binNum2)
   return(entropy::mi.plugin(data.dis))
 }
 
-featureEx <- function(dataH, dataV, binNumH, binNumV, groupSize, max.order){
+#' An "all in one" function that computes several features together
+#' Compute features (featureEx)
+#' @param dataH The horizontal vibration data
+#' @param dataV The vertical vibration data
+#' @param binNumH The binNum parameter for entropy::discretize2d and entropy::discretize function
+#' @param binNumV The binNum parameter for entropy::discretize2d and entropy::discretize function
+#' @param groupSize The size of each group that will be split from the larger data for feature computation
+#' @param max.order The maximum number of coefficients to be computed for the autoregressive model feature
+#' @param ar.threshold The maximum percentage of NA values in the autoregressive model feature
+#' @return A list of features computed from the data. [1]pk2pkH [2]pk2pkV [3]MaxPeakH [4]MaxPeakV [5]rmsH [6]rmsV [7]KurtosisH [8]KurtosisV [9]SkewnessH [10]SkewnessV [11]MutInfo [12]EntropyH [13]EntropyV [14]amPSDH [15]amPSDV [16]LineIntH [17]LineIntV [18]EnergyH [19]EnergyV [20...]arModelH [21...]arModelV
+#' @export
+featureEx <- function(dataH, dataV, binNumH, binNumV, groupSize, max.order, ar.threshold){
   boundSeq = seq(from=1,to = (length(dataH)+1), by = groupSize)
   len = length(boundSeq)-1
   peak2peakH = numeric()
@@ -143,36 +178,28 @@ featureEx <- function(dataH, dataV, binNumH, binNumV, groupSize, max.order){
     EnergyH = c(EnergyH,partFeatures$energyH)
     EnergyV = c(EnergyV,partFeatures$energyV)
   }
+  lenAR = length(arMV[,1])
   done = c(0,0)
-  for(i in 0:(max.order-1)){
-    index = max.order - i
-    lenH = length(which(is.na(as.data.frame(arMH)[,index])))
-
-    if((lenH == len)&&(done[1] == 0))
-      arMH = arMH[,-index]
-    else
-      done[1] = 1
-
-    lenV = length(which(is.na(arMV[,index])))
-    if((lenV == len)&&(done[2] == 0))
-      arMV = arMV[,-index]
-    else
-      done[2] = 1
+  for(i in 1:max.order){
+    if(done[1]==0){
+      zerosH = length(which(is.na(arMH[,i])))/lenAR
+      if(zerosH > ar.threshold){
+        arMH = arMH[,-(i:max.order)]
+        done[1] = 1
+      }
+    }
+    if(done[2]==0){
+      zerosV = length(which(is.na(arMV[,i])))/lenAR
+      if(zerosV > ar.threshold){
+        arMV = arMV[,-(i:max.order)]
+        done[2] = 1
+      }
+    }
     if((done[1]==1)&&(done[2]==1))
       break
   }
   arMH[is.na(arMH)] = 0
   arMV[is.na(arMV)] = 0
-  # p2p = data.frame(Horizontal = peak2peakH, Vertical = peak2peakV)
-  # maxPeak = data.frame(Horizontal = maxPeakH, Vertical = maxPeakV)
-  # RMS = data.frame(Horizontal = RMSH, Vertical = RMSV)
-  # Kurtosis = data.frame(Horizontal = KurtosisH, Vertical = KurtosisV)
-  # Skewness = data.frame(Horizontal = SkewnessH, Vertical = SkewnessV)
-  # Entropy = data.frame(Horizontal = EntropyH, Vertical = EntropyV)
-  # arithmeticMeanPSD = data.frame(Horizontal = amPSDH, Vertical = amPSDV)
-  # LineIntegral = data.frame(Horizontal = LIH, Vertical = LIV)
-  # arModels = data.frame(Horizontal = arMH, Vertical = arMV)
-  # Energy = data.frame(Horizontal = EnergyH, Vertical = EnergyV)
   result = list(peak2peakH,peak2peakV,maxPeakH,maxPeakV,RMSH,RMSV,KurtosisH,KurtosisV,SkewnessH,SkewnessV,MI,EntropyH,EntropyV,amPSDH,amPSDV,LIH,LIV,EnergyH,EnergyV)
   arResultH = as.list(as.data.frame(arMH))
   arResultV = as.list(as.data.frame(arMV))
@@ -182,6 +209,12 @@ featureEx <- function(dataH, dataV, binNumH, binNumV, groupSize, max.order){
 }
 # [1]pk2pkH [2]pk2pkV [3]MaxPeakH [4]MaxPeakV [5]rmsH [6]rmsV [7]KurtosisH [8]KurtosisV [9]SkewnessH [10]SkewnessV [11]MutInfo [12]EntropyH [13]EntropyV [14]amPSDH [15]amPSDV [16]LineIntH [17]LineIntV [18]EnergyH [19]EnergyV [20...]arModelH [21...]arModelV
 
+#' Compute the (1 - Symmetrical uncertainty) of the features
+#' @title Symmetrical Uncertainty (SU)
+#' @param features A list of features, the output of the 'featureEx' function
+#' @param binNum The binNum parameter for entropy::discretize2d and entropy::discretize function
+#' @return A matrix with the SU values of each feature against each other
+#' @export
 SU <- function(features,binNum){
   len = length(features)
   entropy = numeric()
@@ -210,6 +243,14 @@ SU <- function(features,binNum){
   return(1-result)
 }
 
+#' Compute the Root-Mean-Squared Error of the data given a linear model
+#' @title Root-Mean-Squared Error (RMSE)
+#' @param intercept The intercept of the linear model
+#' @param coefficient The coefficient of the linear model
+#' @param data The data for computing the RMSE
+#' @param clusterNum The possible indices for the cluster having its RMSE computed
+#' @return the RMSE of the data given the linear model
+#' @export
 RMSE <- function(intercept, coefficient, data, clusterNum){
   len = length(clusterNum)
   sum = 0
@@ -221,6 +262,10 @@ RMSE <- function(intercept, coefficient, data, clusterNum){
   return(result)
 }
 
+#' Use the L-method to compute the knee (optimal number of clusters) for the hierarchical clustering
+#' @title L-method (knee)
+#' @param su The matrix of SU values, output of the 'SU' function
+#' @return The knee (optimal number of clusters) of the hierarchical clustering
 #' @export
 knee <- function(su){
   hc = hclust(as.dist(su))
@@ -238,4 +283,214 @@ knee <- function(su){
     RMSEc = c(RMSEc,RMSE)
   }
   return(which.min(RMSEc)+1)
+}
+
+#' Find the best cluster within the features using SOM
+#' @title Choose Best Cluster (chooseCluster)
+#' @param features A list of the computed features, output of the 'featureEx' function
+#' @param su The SU values of the features, output of the 'SU' function
+#' @param knee The knee of the clusters, output of the 'knee' function
+#' @param xdim The parameter for the function som::som
+#' @param ydim The parameter for the function som::som
+#' @return The indices of the features of the chosen cluster
+#' @export
+chooseCluster <- function(features, su, knee, xdim, ydim){
+  hc = hclust(as.dist(su))
+  ct = cutree(hc,k=knee)
+  qerrorScore = numeric()
+  for(i in 1:knee){
+    df = as.data.frame(features[which(ct==i)])
+    df = unname(df)
+    if(length(df)!=1){
+      s = som(df,xdim,ydim)
+      qerrorScore = c(qerrorScore,qerror(s))
+    }
+    else
+      qerrorScore = c(qerrorScore, Inf)
+  }
+  return(which(ct==which.min(qerrorScore)))
+}
+
+#' Principal Component Analysis
+#' @title Principal Component Analysis (pca)
+#' @param features A list of the computed features, output of the 'featureEx' function
+#' @param clustersChosen A vector containing the indices of the clusters chosen, output of the 'chooseCluster' function
+#' @return The set of data from PCA with the lowest standard deviation
+#' @export
+pca <- function(features, clustersChosen){
+  df = as.data.frame(features[clustersChosen])
+  df = unname(df)
+  pc = prcomp(df,scale. = TRUE)
+  return(pc$x[,which.min(pc$sdev)])
+}
+
+#' Apply Empirical Mode Decomposition to the data
+#' @title Empirical Mode Decomposition (EMDR)
+#' @param data The data to be decomposed using EMD
+#' @return The EMD object from the EMD R package
+#' @export
+emdR <- function(data){
+  result = EMD::emd(data)
+  return(result)
+}
+
+plotResidual <- function(emd){
+  len = length(emd$residue)
+  x = 1:len
+  y = emd$residue
+  df = data.frame(y,x)
+  lo = loess(y~x,df)
+  plot.ts(y)
+  lines(predict(lo),col = "red",lwd = 2)
+}
+
+get.fftloess <- function(data, groupSize){
+
+  boundSeq = seq(from=1,to = (length(data)+1), by = groupSize)
+  len = length(boundSeq) - 1
+  time = 1:groupSize
+  lowerBound = boundSeq[1]
+  upperBound = boundSeq[2] - 1
+  fft = abs(fft(data[lowerBound:upperBound]))
+  lo = predict(loess(fft~time))
+
+  for(i in 2:len){
+    lowerBound = boundSeq[i]
+    upperBound = boundSeq[i+1] - 1
+    fft = abs(fft(data[lowerBound:upperBound]))
+    lo1 = predict(loess(fft~time))
+    lo = rbind(lo,lo1)
+  }
+
+  return(lo)
+}
+
+get.fftloess.skip <- function(data, groupSize,smoothSampleNum){
+  boundSeq = seq(from=1,to = (length(data)+1), by = groupSize)
+  len = length(boundSeq) - 1
+  boundSeq2 = seq(from = 1, to = length(data),length.out = smoothSampleNum)
+  time = 1:groupSize
+  lowerBound = boundSeq[1]
+  upperBound = boundSeq[2] - 1
+  fft = abs(fft(data[lowerBound:upperBound]))
+  lo = predict(loess(fft~time))
+
+  for(i in 2:len){
+    lowerBound = boundSeq[i]
+    upperBound = boundSeq[i+1] - 1
+    fft = abs(fft(data[lowerBound:upperBound]))
+    lo1 = predict(loess(fft~time))[boundSeq2]
+    lo = rbind(lo,lo1)
+  }
+  return(lo)
+}
+
+get.fftloess.d10 <- function(data, groupSize){
+  boundSeq = seq(from=1,to = (length(data)+1), by = groupSize)
+  len = length(boundSeq) - 1
+  winSize = as.integer(groupSize/20)
+  lb = as.integer(groupSize/2-winSize)
+  ub = as.integer(groupSize/2+winSize)
+  time = 1:groupSize
+  lowerBound = boundSeq[1]
+  upperBound = boundSeq[2] - 1
+  fft = abs(fft(data[lowerBound:upperBound]))
+  lo = predict(loess(fft~time))[lb:ub]
+
+  for(i in 2:len){
+    lowerBound = boundSeq[i]
+    upperBound = boundSeq[i+1] - 1
+    fft = abs(fft(data[lowerBound:upperBound]))
+    lo1 = predict(loess(fft~time))[lb:ub]
+    lo = rbind(lo,lo1)
+  }
+  return(lo)
+}
+
+selfRUL.h2o.fftloess <- function(data,groupSize,degradStartPercent){
+  fftloess = get.fftloess(data,groupSize)
+  rul = c(rep(nrow(fftloess),as.integer(nrow(fftloess)*degradStartPercent)),seq(from=(nrow(fftloess)), to = 1, length.out = (nrow(fftloess))-as.integer(nrow(fftloess)*degradStartPercent)))
+  fftloess.df = data.frame(rul = rul, fftloess)
+  fftloess.hex = as.h2o(fftloess.df)
+  fftloess.dl = h2o.deeplearning(x = 2:(ncol(fftloess)),y = 1,training_frame = fftloess.hex)
+  predictions <- h2o.predict(fftloess.dl, fftloess.hex)
+  predictions = as.data.frame(predictions)
+  plot.ts(predictions$predict)
+}
+
+selfRUL.h2o.fftloess.logic <- function(data,groupSize,degradStartPercent){
+  fftloess = get.fftloess(data,groupSize)
+  rul = c(rep(TRUE,as.integer(nrow(fftloess)*degradStartPercent)),rep(FALSE,(nrow(fftloess))-as.integer(nrow(fftloess)*degradStartPercent)))
+  fftloess.df = data.frame(rul = rul, fftloess)
+  fftloess.hex = as.h2o(fftloess.df)
+  fftloess.dl = h2o.deeplearning(x = 2:(ncol(fftloess)),y = 1,training_frame = fftloess.hex)
+  predictions <- h2o.predict(fftloess.dl, fftloess.hex)
+  predictions = as.data.frame(predictions)
+  plot.ts(predictions$predict)
+}
+
+selfRUL.h2o.fftloess.logic.skip <- function(data,groupSize,degradStartPercent,smoothSampleNum){
+  fftloess = get.fftloess.skip(data,groupSize,smoothSampleNum)
+  #rul = c(rep(nrow(fftloess),as.integer(nrow(fftloess)*degradStartPercent)),seq(from=(nrow(fftloess)), to = 1, length.out = (nrow(fftloess))-as.integer(nrow(fftloess)*degradStartPercent)))
+  rul = c(rep(TRUE,as.integer(nrow(fftloess)*degradStartPercent)),rep(FALSE,(nrow(fftloess))-as.integer(nrow(fftloess)*degradStartPercent)))
+  fftloess.df = data.frame(rul = rul, fftloess)
+  fftloess.hex = as.h2o(fftloess.df)
+  fftloess.dl = h2o.deeplearning(x = 2:(ncol(fftloess)),y = 1,training_frame = fftloess.hex)
+  predictions <- h2o.predict(fftloess.dl, fftloess.hex)
+  predictions = as.data.frame(predictions)
+  plot.ts(predictions$predict)
+}
+
+selfRUL.h2o.fftloess.d10 <- function(data,groupSize,degradStartPercent){
+  fftloess = get.fftloess.d10(data,groupSize)
+  rul = c(rep(nrow(fftloess),as.integer(nrow(fftloess)*degradStartPercent)),seq(from=(nrow(fftloess)), to = 1, length.out = (nrow(fftloess))-as.integer(nrow(fftloess)*degradStartPercent)))
+  fftloess.df = data.frame(rul = rul, fftloess)
+  fftloess.hex = as.h2o(fftloess.df)
+  fftloess.dl = h2o.deeplearning(x = 2:(ncol(fftloess)),y = 1,training_frame = fftloess.hex)
+  predictions <- h2o.predict(fftloess.dl, fftloess.hex)
+  predictions = as.data.frame(predictions)
+  plot.ts(predictions$predict)
+}
+
+plot.fftloess <- function(data, groupSize){
+
+  fft1 = abs(fft(data[1:groupSize]))
+  fft3 = abs(fft(data[(length(data)-groupSize+1):(length(data))]))
+  if(groupSize%%2 == 0){
+    fft2 = abs(fft(data[(length(data)/2 - groupSize/2 + 1):(length(data)/2 + groupSize/2)]))
+  }
+  else{
+    fft2 = abs(fft(data[(length(data)/2 - groupSize/2 + 0.5):(length(data)/2) + groupSize/2 - 0.5]))
+  }
+  time = 1:groupSize
+  lo1 = predict(loess(fft1~time))
+  lo2 = predict(loess(fft2~time))
+  lo3 = predict(loess(fft3~time))
+  plot.ts(fft1)
+  lines(lo1,col= "red")
+  lines(lo2,col= "blue")
+  lines(lo3,col= "green")
+}
+
+plot.fftloess.d10 <- function(data, groupSize){
+  fft1 = abs(fft(data[1:groupSize]))
+  fft3 = abs(fft(data[(length(data)-groupSize+1):(length(data))]))
+  if(groupSize%%2 == 0){
+    fft2 = abs(fft(data[(length(data)/2 - groupSize/2 + 1):(length(data)/2 + groupSize/2)]))
+  }
+  else{
+    fft2 = abs(fft(data[(length(data)/2 - groupSize/2 + 0.5):(length(data)/2) + groupSize/2 - 0.5]))
+  }
+  winSize = as.integer(groupSize/20)
+  lb = as.integer(groupSize/2-winSize)
+  ub = as.integer(groupSize/2+winSize)
+  time = 1:groupSize
+  lo1 = predict(loess(fft1~time))[lb:ub]
+  lo2 = predict(loess(fft2~time))[lb:ub]
+  lo3 = predict(loess(fft3~time))[lb:ub]
+  plot.ts(fft1)
+  lines(lb:ub,lo1,col= "red")
+  lines(lb:ub,lo2,col= "blue")
+  lines(lb:ub,lo3,col= "green")
+
 }
