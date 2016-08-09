ec219288468271215cfd56300664907fffea2c8b
allenhuang97 <allen.weixi.huang@gmail.com>
2016-08-09 14:36:11 -0400

    energy rul

--- R/RULWPDdl.R
+++ R/RULWPDdl.R
@@ -9,34 +9,32 @@
 #' @return A deeplearning model from h2o of the wpd nodal energies
 #' @export
 createModel.h2o.wpd.rul <- function(data,exp2,levels,degradStartPercent,nPast = 0,cores=NULL){
+  energy = getEnergies.wpd(data,exp2,levels)
+  time = 1:nrow(energy)
   registerDoMC(cores = cores)
-  energy.df = foreach(ii = 1:length(data), .combine = 'rbind') %dopar% {
-    energy = getEnergies.wpd(data[[ii]],exp2,levels)
-    time = 1:nrow(energy)
-    # registerDoMC(cores = cores)
-    energy = foreach(i = 1:ncol(energy),.combine = 'cbind',.inorder = TRUE) %do% {
-      predict(loess(energy[,i]~time))
-    }
-    # for(i in 1:ncol(energy))
-    #   energy[,i] = predict(loess(energy[,i]~time))
-
-    if(nPast != 0){
-      rowNum = nrow(energy)-nPast
-      registerDoMC(cores = cores)
-      energy = foreach(i = 0:rowNum, .combine = 'rbind', .inorder = TRUE) %dopar% {
-        lb = i+1
-        ub = nPast+i
-        as.vector(energy[lb:ub,])
-      }
-      energy = unname(energy)
-    }
-    rul = c(rep(nrow(energy),as.integer(nrow(energy)*degradStartPercent)),seq(from=(nrow(energy)), to = 1, length.out = (nrow(energy))-as.integer(nrow(energy)*degradStartPercent)))
-    energy.df = data.frame(energy,rul = rul)
+  energy = foreach(i = 1:ncol(energy),.combine = 'cbind',.inorder = TRUE) %dopar% {
+    predict(loess(energy[,i]~time))
   }
-  energy.hex = as.h2o(energy.df)
-  energy.dl = h2o.deeplearning(x = 1:(2^levels),y = (ncol(energy.df)),training_frame = energy.hex)
-  return(list(model = energy.dl,exp2 = exp2, levels = levels,nPast = nPast))
+  # for(i in 1:ncol(energy))
+  #   energy[,i] = predict(loess(energy[,i]~time))
+  if(nPast != 0){
+    rowNum = nrow(energy)-nPast
+    registerDoMC(cores = cores)
+    energy = foreach(i = 0:rowNum, .combine = 'rbind', .inorder = TRUE) %dopar% {
+      lb = i+1
+      ub = nPast+i
+      as.vector(energy[lb:ub,])
+    }
+    energy = unname(energy)
+  }
+  rul = c(rep(nrow(energy),as.integer(nrow(energy)*degradStartPercent)),seq(from=(nrow(energy)), to = 1, length.out = (nrow(energy))-as.integer(nrow(energy)*degradStartPercent)))
+  energy.df = data.frame(energy,rul = rul)
+  return(energy.df)
 }
+#   energy.hex = as.h2o(energy.df)
+#   energy.dl = h2o.deeplearning(x = 1:(2^levels),y = (ncol(energy.df)),training_frame = energy.hex)
+#   return(list(model = energy.dl,exp2 = exp2, levels = levels,nPast = nPast))
+# }
 
 #' Predict the RUL by using the nodal energies of the WPD
 #' @title WPD RUL (predict.h2o.wpd.rul)
@@ -74,4 +72,18 @@ predict.h2o.wpd.rul <- function(data,model,cores = NULL){
   pred = as.data.frame(pred)
   return(pred$predict)
 }
+plot.ts(predict.h2o.wpd.rul(h1_1,modelH1_3_p0))
+plot.ts(predict.h2o.wpd.rul(h1_2,modelH1_3_p0))
+plot.ts(predict.h2o.wpd.rul(h1_3,modelH1_3_p0))
+plot.ts(predict.h2o.wpd.rul(h1_4,modelH1_3_p0))
+plot.ts(predict.h2o.wpd.rul(h1_5,modelH1_3_p0))
+plot.ts(predict.h2o.wpd.rul(h1_6,modelH1_3_p0))
+plot.ts(predict.h2o.wpd.rul(h1_7,modelH1_3_p0))
 
+df1 = createModel.h2o.wpd.rul(h1_1,8,3,0)
+df2 = createModel.h2o.wpd.rul(h1_2,8,3,0)
+df3 = createModel.h2o.wpd.rul(h1_3,8,3,0)
+df4 = createModel.h2o.wpd.rul(h1_4,8,3,0)
+df5 = createModel.h2o.wpd.rul(h1_5,8,3,0)
+df6 = createModel.h2o.wpd.rul(h1_6,8,3,0)
+df7 = createModel.h2o.wpd.rul(h1_7,8,3,0)
